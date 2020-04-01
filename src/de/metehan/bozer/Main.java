71645a884b39be17ea4beffb08eaaf6d24a92437
Mete-Hân <Mete-Han.Bozer@Student.HTW-Berlin.de>
2020-04-01 23:34:03 +0200

    Conclusion to exercise
    Using git over IDEs sucks

--- src/de/metehan/bozer/Main.java
+++ src/de/metehan/bozer/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("\nUebung HTw PG2: Mthn-Karga");
+	System.out.println("\n------ Erkenntnis: -----------------------");
 	System.out.println("\nIDEs zum Arbeiten mit Git sind doof!");
 	System.out.println("\nWork like a pro and use tools made for it!");
 	System.out.println("\n;-)");
+	System.out.println("\n------------------------------------------");
     }
 }
