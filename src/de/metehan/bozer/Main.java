9ac7a9a84b3d29a126eab859b5bf606a6c3ff188
Mete-Hân <Mete-Han.Bozer@Student.HTW-Berlin.de>
2020-04-01 23:29:38 +0200

    Created System.out

--- src/de/metehan/bozer/Main.java
+++ src/de/metehan/bozer/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("\nUebung HTw PG2: Mthn-Karga");
+	System.out.println("\nIDEs zum Arbeiten mit Git sind doof!");
+	System.out.println("\nWork like a pro and use tools made for it!");
+	System.out.println("\n;-)");
     }
 }
