13ac9efd806cf2b30b35d27dfd31f542a83c399f
Mete-Hân <Mete-Han.Bozer@Student.HTW-Berlin.de>
2020-04-01 23:22:41 +0200

    Created System.out

--- src/de/metehan/bozer/Main.java
+++ src/de/metehan/bozer/Main.java
@@ -3,6 +3,6 @@ package de.metehan.bozer;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("\nUebung HTw PG2: Mthn-Karga");
     }
 }
