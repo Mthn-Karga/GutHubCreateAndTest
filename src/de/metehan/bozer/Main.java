fdb1e44b50e8a24b581f2ae6cf9399cc26d273f7
Mete-Hân <Mete-Han.Bozer@Student.HTW-Berlin.de>
2020-04-01 23:40:28 +0200

    Correct typo

--- src/de/metehan/bozer/Main.java
+++ src/de/metehan/bozer/Main.java
@@ -3,7 +3,7 @@ package de.metehan.bozer;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("\nUebung HTw PG2: Mthn-Karga");
+	System.out.println("\nUebung HTW PG2: Mthn-Karga");
 	System.out.println("\n------ Erkenntnis: -----------------------");
 	System.out.println("\nIDEs zum Arbeiten mit Git sind doof!");
 	System.out.println("\nWork like a pro and use tools made for it!");
